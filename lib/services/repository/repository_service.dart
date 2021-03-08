d44ed305fbad3be82d57895a0e04c4d6252a42a4
Jesse Dirisu <dirisujesse@gmail.com>
2021-03-08 14:07:20 +0100

    Fixes cacheservice key mismatch
    Signed-off-by: Jesse Dirisu <dirisujesse@gmail.com>

--- lib/services/repository/repository_service.dart
+++ lib/services/repository/repository_service.dart
@@ -8,25 +8,33 @@ mixin CacheService {
   /// `Fetch` serves as an helper to read reponses stored to the cache
   /// It takes a `DioError` object as input from which it extracts a key
   /// matching a (potentially) pre cached response.
-  /// 
+  ///
   /// If no match is found the inputed error is thrown
-  /// 
+  ///
   /// It serves to allow for offline usage of `Molteo_Books`
   ///
   Future<dynamic> fetch(DioError e) async {
-    final error = {"message": e?.response?.data ?? e?.error ?? e, "data": e?.response?.data, "statusCode": e?.response?.statusCode};
+    print("o ma sanku ni");
+    final error = {
+      "message": e?.response?.data ?? e?.error ?? e,
+      "data": e?.response?.data,
+      "statusCode": e?.response?.statusCode
+    };
     try {
       final key =
           "${e.request.baseUrl}::${e.request.path}::${e.request.queryParameters}";
+      print(key);
       final data = LocalStorage.getItem(
-        key,
+        key.toLowerCase(),
         defaultValue: false,
       );
       if (data == false) {
         throw error;
       }
+      print("data: $data");
       return data;
     } catch (_) {
+      print(_);
       throw error;
     }
   }
@@ -37,16 +45,21 @@ mixin CacheService {
   /// `CacheResponse` serves as an helper to read data off responses from network calls
   /// It takes a `Response` object as input from which it extracts response data to be cached
   /// as well as a key by which to subsequently read the cached response
-  /// 
+  ///
   /// If a failure to cache occurs the computation silently fails
   ///
   cacheResponse(Response req) async {
     try {
+      final key =
+          "${req.request.baseUrl}::${req.request.path}::${req.request.queryParameters}";
+      print(key);
       await LocalStorage.setItem(
-        key:
-            "${req.request.baseUrl}/${req.request.path}/${req.request.queryParameters}",
+        key: key.toLowerCase(),
         value: req.data,
       );
-    } catch (_) {}
+      print("saved data");
+    } catch (_) {
+      print(_);
+    }
   }
 }
